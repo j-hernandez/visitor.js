e50a9d6320623eec9eb0d46ca39b788b855686de
Jonas Hermsmeier <jhermsmeier@googlemail.com>
2012-03-06 10:41:56 +0100

    Append script to <body> in `util.loadScript()`

--- visitor.js
+++ visitor.js
@@ -479,7 +479,7 @@
         script = document.createElement( 'script' );
         script.type = 'text/javascript';
         script.src  = url;
-        document.getElementsByTagName( 'head' )[0].appendChild( script );
+        document.body.appendChild( script );
       }
       
     };
